4ddbf5da8d54f133be492e36d2d7a4487f1f7cdd
Hanzhiyun <hanzhiyun1995@qq.com>
2017-02-01 17:24:33 +0800

    write web app frame

--- www/app.py
+++ www/app.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from datetime import datetime
+from aiohttp import web
+
 __author__ = 'Hanzhiyun'
 
+
+def index(request):
+    return web.Response(content_type='text/html', body=b'<h1>Awesome</h1>')
+
+
+def hello(request):
+    text = '<h1>hello, %s!</h1>' % request.match_info['name']
+    return web.Response(content_type='text/html', body=text.encode('utf-8'))
+
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    app.router.add_route('GET', '/hello/{name}', hello)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
