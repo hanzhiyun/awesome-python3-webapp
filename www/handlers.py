d207c8fd5337759a449b3fa810f74bf19c12128e
Hanzhiyun <1160588449@qq.com>
2017-02-18 21:34:09 +0800

    new python files for web frame

---
+++ www/handlers.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date    : 2017-02-18 19:05:40
+# @Author  : Han Zhiyun (hanzhiyun1995@qq.com)
+
+
+from coroweb import get, post
+from aiohttp import web
+
+@get('/blog')
+async def handler_url_blog(request):
+    body='<h1>Awesome: /blog</h1>'
+    return body
+
+@get('/greeting')
+async def handler_url_greeting(*,name,request):
+    body='<h1>Awesome: /greeting %s</h1>'%name
+    return body
+
+@get('/input')
+async def handler_url_input(request):
+    body='<form action="/result" method="post">E-mail: <input type="email" name="user_email" /><input type="submit" /></form>'
+    return body
+
+@post('/result')
+async def handler_url_result(*,user_email,request):
+    body='<h1>您输入的邮箱是%s</h1>'%user_email
+    return body
+
+@get('/index')
+async def handler_url_index(request):
+    body='<h1>Awesome: /index</h1>'
+    return body
+
+@get('/create_comment')
+async def handler_url_create_comment(request):
+    body='<h1>Awesome: /create_comment</h1>'
+    return body
+    
