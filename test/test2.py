c62e836a20f9cf81bb250c62aed537954211657e
Hanzhiyun <1160588449@qq.com>
2017-02-18 21:35:23 +0800

    test function of inspect.signature and parameter

--- test/test2.py
+++ test/test2.py
@@ -10,5 +10,28 @@ def greet_me(**fuckhahah):
         print(my_info.get(key))
         print('Fuck','None')
 my_info={'name':'sly','email':'slysly759@gmail.com'}
-greet_me(**my_info)
+# greet_me(**my_info)
 # print(my_info.items())
+
+
+
+import inspect  
+def foo(a, *,b:int,**kw):
+	pass
+
+
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    print(params)
+    print(inspect.signature(fn))
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
+
+
+if __name__ == "__main__":
+	print('test')
+	print(get_required_kw_args(foo))
+
